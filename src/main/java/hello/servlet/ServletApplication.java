bf1e7f57d9c68e01fb315d31a29204f858b36049
updown-kurly <sangha.kim@kurlycorp.com>
2021-07-24 00:06:30 +0900

    add: 직접 서블릿 생성해보고 index.html 파일 만들어 띄어보기

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 하위패키지에 있는 서블릿 찾아서 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
